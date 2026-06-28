7b3626232082b17c8a215ba0a48ee1837d822128
nitinniwas-dev <nitinshah74308@gmail.com>
2026-06-28 18:30:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,FileResponse
 from pydantic import BaseModel
 import pickle
 import numpy as np
